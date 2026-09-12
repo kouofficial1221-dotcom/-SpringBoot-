58840f510adf0dde54177c37559749a009853b4e
exper <exper@Ponnnu1221>
2026-09-12 14:22:47 +0900

    JUnitのテストの順序を決めるアノテーションを検証。

--- ch03-java/src/test/java/com/springbootbook/ch03java/OrderTest.java
+++ ch03-java/src/test/java/com/springbootbook/ch03java/OrderTest.java
@@ -7,5 +7,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderTest {
-
+	//テストの数に限らず、1回だけ最初にテストをする
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("beforeAll()");
+	}
+	
+	//テストの数に限らず、1回だけ最後にテストをする
+	@AfterAll
+	static void afterAll() {
+		System.out.println("afterAll()");
+	}
+	
+	//テストの回数分、テストの前に実施する
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("beforeEach()");
+	}
+	
+	//テストの回数分、テストの後に実施する
+	@AfterEach
+	void afterEach() {
+		System.out.println("afterEach()");
+	}
+	
+	@Test
+	void test01() {
+		System.out.println("test01()");
+	}
+	
+	@Test
+	void test02() {
+		System.out.println("test02()");
+	}
 }
