ca3dfb0462d838996d387d3a9c621fcefc1383c0
exper <exper@Ponnnu1221>
2026-09-13 11:22:57 +0900

    DIコンテナ・BeanインスタンスのMethodを呼び出す方法を実践。

--- ch04-01-component-scan/src/main/java/com/springbootbook/ch0401componentscan/pack1/Sample1.java
+++ ch04-01-component-scan/src/main/java/com/springbootbook/ch0401componentscan/pack1/Sample1.java
@@ -2,6 +2,9 @@ package com.springbootbook.ch0401componentscan.pack1;
 
 import org.springframework.stereotype.Component;
 
+@Component
 public class Sample1 {
-
+	public void method1(){
+		System.out.println("method1()を実行します。");
+	}
 }
