30acd2084999b6cb1dfc3df1aff2c10753626f65
exper <exper@Ponnnu1221>
2026-09-13 15:39:39 +0900

    JDBCを使ってDatabaseの操作を行った。

--- ch05-database/src/main/java/com/springbootbook/ch05database/persistence/repository/IdolRepository.java
+++ ch05-database/src/main/java/com/springbootbook/ch05database/persistence/repository/IdolRepository.java
@@ -1,14 +1,91 @@
 package com.springbootbook.ch05database.persistence.repository;
 
-import com.springbootbook.ch05database.persistence.entity.Idol;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import com.springbootbook.ch05database.persistence.entity.Idol;
 
+@Repository
 public class IdolRepository {
 
+	private final JdbcClient jdbcClient;
+
+	public IdolRepository(JdbcClient jdbcClient) {
+		this.jdbcClient = jdbcClient;
+	}
+
+	public Optional<Idol> selectById(Integer id) {
+		Optional<Idol> idolOptional = jdbcClient.sql("""
+				SELECT id, name, birthday, blood_type
+				FROM idol WHERE id = :id
+				""")
+				.param("id", id)
+				.query(new DataClassRowMapper<>(Idol.class))
+				.optional();
+		return idolOptional;
+
+	}
+
+	public List<Idol> selectByNameOrderById(String nameKeyword) {
+		List<Idol> idolList = jdbcClient.sql("""
+				SELECT id, name ,birthday, blood_type
+				FROM idol WHERE name LIKE :name ORDER BY id
+				""")
+				.param("name", "%" + nameKeyword + "%")
+				.query(new DataClassRowMapper<>(Idol.class))
+				.list();
+		return idolList;
+	}
+
+	public int countById(Integer id) {
+		int count = jdbcClient.sql("""
+				SELECT COUNT(*) FROM idol
+				WHERE id = :id
+				""")
+				.param("id", id)
+				.query(Integer.class)
+				.single();
+		return count;
+	}
+
+	public int update(Idol idol) {
+		int rows = jdbcClient.sql("""
+				UPDATE idol
+				SET name = :name, birthday = :birthday, blood_type =  :blood_type
+				""")
+				.param("name", idol.name())
+				.param("birthday", idol.birthday())
+				.param("blood_type", idol.bloodType().name())
+				.param("id", idol.id())
+				.update();
+		return rows;
+	}
+
+	public int delete(Integer id) {
+		int rows = jdbcClient.sql("""
+				DELETE FROM idol WHERE id = :id
+				""")
+				.param("id", id)
+				.update();
+		return rows;
+	}
+
+	public Idol insert(Idol idol) {
+		GeneratedKeyHolder KeyHolder = new GeneratedKeyHolder();
+		jdbcClient.sql("""
+				INSERT INTO idol(name, birthday, blood_type)
+				VALUE(:name, :birthday, :blood_type)
+				""")
+				.param("name", idol.name())
+				.param("birthday", idol.birthday())
+				.param("blood_type", idol.bloodType().name())
+				.update(KeyHolder, "id");
+		int newId = KeyHolder.getKey().intValue();
+		return new Idol(newId, idol.name(), idol.birthday(), idol.bloodType());
+	}
 }
