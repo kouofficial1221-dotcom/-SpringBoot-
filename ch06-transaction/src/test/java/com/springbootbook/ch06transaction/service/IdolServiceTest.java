5c26dfabd26ef2eb8a2f49da19a1b3aedb550b7a
exper <exper@Ponnnu1221>
2026-09-14 20:22:39 +0900

    Mockitoを利用してJUnitのテストコードを記述※量が多すぎたため一部のみ記載

--- ch06-transaction/src/test/java/com/springbootbook/ch06transaction/service/IdolServiceTest.java
+++ ch06-transaction/src/test/java/com/springbootbook/ch06transaction/service/IdolServiceTest.java
@@ -1,20 +1,61 @@
 package com.springbootbook.ch06transaction.service;
 
-import com.springbootbook.ch06transaction.persistence.entity.BloodType;
-import com.springbootbook.ch06transaction.persistence.entity.Idol;
-import com.springbootbook.ch06transaction.persistence.repository.IdolRepository;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+import com.springbootbook.ch06transaction.persistence.entity.BloodType;
+import com.springbootbook.ch06transaction.persistence.entity.Idol;
+import com.springbootbook.ch06transaction.persistence.repository.IdolRepository;
 
 public class IdolServiceTest {
-
+	IdolService idolService;
+	
+	IdolRepository idolRepository;
+	
+	@BeforeEach
+	void beforeEach() {
+		idolRepository = mock(IdolRepository.class);
+		idolService = new IdolService(idolRepository);
+		
+		@Nested
+		@DisplayName("findById()")
+		class FindByIdTest{
+			@Test
+			@DisplayName("存在するIDを指定すると、該当するアイドルを取得できる")
+			void exists() {
+				Optional<Idol> expected = Optional.of(new Idol(1,"ふじの　もも",LocalDate.of(2001, 1, 1),BloodType.A));
+				doReturn(expected).when(idolRepository).selectById(anyInt());
+				Optional<Idol> actual = idolService.	findById(1);
+				assertEquals(expected,actual);
+			}
+			
+			@Test
+			@DisplayName("存在しないIDを指定すると、からのOptionalが返る")
+			void doesnotExist() {
+				Optional<Idol> expected = Optional.empty();
+				doReturn(expected).when(idolRepository).selectById(anyInt());
+				Optional<Idol>actual = idolService.findById(1);
+				assertEquals(expected,actual);
+			}
+			
+			@Test
+			@DisplayName("存在しないIDを指定すると、空のOptionalが返る")
+			void doesNotExist() {
+				Optional<Idol> expected = Optional.empty();
+				doReturn(expected).when(idolRepository).selectById(anyInt());
+				Optional<Idol> actual = idolService.findById(1);
+				assertEquals(expected, actual);
+			}
+			
+		}
+	}
 }
