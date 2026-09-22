0afd1a8393f8d71cfbd963d2b9c39d9f7e4752ca
kouofficial1221-dotcom <kou.official1221@gmail.com>
2026-09-22 21:47:01 +0900

    Mockがうまくいかない※原因究明中

--- ch08-validation/src/test/java/com/springbootbook/ch08validation/web/controller/IdolControllerTest.java
+++ ch08-validation/src/test/java/com/springbootbook/ch08validation/web/controller/IdolControllerTest.java
@@ -1,10 +1,8 @@
 package com.springbootbook.ch08validation.web.controller;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+import com.springbootbook.ch08validation.persistence.entity.BloodType;
+import com.springbootbook.ch08validation.persistence.entity.Idol;
+import com.springbootbook.ch08validation.service.IdolService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,7 +12,12 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.springbootbook.ch08validation.service.IdolService;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -58,6 +61,129 @@ public class IdolControllerTest {
     }
 
     @Nested
+    @DisplayName("新規加入画面")
+    class JoinMainTest {
+        @Test
+        @DisplayName("新規加入画面にアクセスすると200 OK")
+        void joinMain() throws Exception {
+            mvc.perform(get("/idol/join"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("idol/join"));
+        }
+    }
+
+    @Nested
+    @DisplayName("新規加入の実行")
+    class JoinTest {
+        @Test
+        @DisplayName("正しい加入アイドルの情報を入力すると、アイドル一覧画面にリダイレクトする")
+        void redirectToIndex() throws Exception {
+            mvc.perform(post("/idol/join")
+                    .param("name", "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２")
+                    .param("birthday", "2006-06-06")
+                    .param("bloodType", "A"))
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(redirectedUrl("/"));
+        }
+
+        @Test
+        @DisplayName("アイドル情報をすべて空で入力すると、新規加入画面に戻る")
+        void empty() throws Exception {
+            mvc.perform(post("/idol/join")
+                            .param("name", "")
+                            .param("birthday", "")
+                            .param("bloodType", ""))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("idol/join"));
+        }
+
+        @Test
+        @DisplayName("33文字以上の名前を入力すると、新規加入画面に戻る")
+        void tooLongName() throws Exception {
+            mvc.perform(post("/idol/join")
+                            .param("name", "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３")
+                            .param("birthday", "2006-06-06")
+                            .param("bloodType", "UNKNOWN"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("idol/join"));
+        }
+    }
+
+    @Nested
+    @DisplayName("修正画面")
+    class FixMainTest {
+        @Test
+        @DisplayName("修正画面にアクセスすると200 OK")
+        void fixMain() throws Exception {
+            doReturn(Optional.of(new Idol(1, "ダミー", LocalDate.of(2001, 1, 1), BloodType.A))).when(idolService).findById(anyInt());
+            mvc.perform(get("/idol/1"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("idol/fix"));
+        }
+
+        @Test
+        @DisplayName("存在しないIDを指定して修正画面にアクセスすると、エラー画面に遷移する")
+        void doesNotExistMain() throws Exception {
+            doReturn(Optional.empty()).when(idolService).findById(anyInt());
+            mvc.perform(get("/idol/999"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("error"));
+        }
+    }
+
+    @Nested
+    @DisplayName("修正の実行")
+    class FixTest {
+        @Test
+        @DisplayName("正しいアイドルの情報を入力すると、アイドル一覧画面にリダイレクトする")
+        void redirectToIndex() throws Exception {
+            doReturn(true).when(idolService).exists(anyInt());
+            mvc.perform(post("/idol/1")
+                            .param("name", "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２")
+                            .param("birthday", "2006-06-06")
+                            .param("bloodType", "A"))
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(redirectedUrl("/"));
+        }
+
+        @Test
+        @DisplayName("アイドル情報をすべて空で入力すると、修正画面に戻る")
+        void empty() throws Exception {
+            doReturn(true).when(idolService).exists(anyInt());
+            mvc.perform(post("/idol/1")
+                            .param("name", "")
+                            .param("birthday", "")
+                            .param("bloodType", ""))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("idol/fix"));
+        }
+
+        @Test
+        @DisplayName("33文字以上の名前を入力すると、修正画面に戻る")
+        void tooLongName() throws Exception {
+            doReturn(true).when(idolService).exists(anyInt());
+            mvc.perform(post("/idol/1")
+                            .param("name", "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３")
+                            .param("birthday", "2006-06-06")
+                            .param("bloodType", "UNKNOWN"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("idol/fix"));
+        }
+
+        @Test
+        @DisplayName("存在しないIDを指定して修正を行うと、エラー画面に遷移する")
+        void doesNotExistFix() throws Exception {
+            doReturn(false).when(idolService).exists(anyInt());
+            mvc.perform(post("/idol/999")
+                            .param("name", "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２")
+                            .param("birthday", "2006-06-06")
+                            .param("bloodType", "A"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("error"));
+        }
+    }
+
+    @Nested
     @DisplayName("アイドル卒業")
     class GraduateTest {
         @Test
@@ -68,30 +194,14 @@ public class IdolControllerTest {
                     .andExpect(status().is3xxRedirection())
                     .andExpect(redirectedUrl("/"));
         }
-    
+
         @Test
         @DisplayName("存在しないIDを指定してアイドル卒業を行うと、エラー画面に遷移する")
-        void doesNotExist() throws Exception{
-        		doReturn(false).when(idolService).exists(anyInt());
-        		mvc.perform(post("/idol/graduate/1"))
-        		.andExpect(status().isOk())
-        		.andExpect(view().name("error"));
+        void doesNotExist() throws Exception {
+            doReturn(false).when(idolService).exists(anyInt());
+            mvc.perform(post("/idol/graduate/1"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("error"));
         }
     }
-    
-    @Nested
-    @DisplayName("新規加入画面")
-    class joinMainTest{
-    		@Test
-    		@DisplayName("新規加入画面にアクセスすると200 OK")
-    		void joinMain() throws Exception{
-    			mvc.perform(get("/idol/join"))
-    					.andExpect(status().isOk())
-    					.andExpect(view().name("idol/join"));
-    		}
-    }
-    
-
-    
-	
 }
